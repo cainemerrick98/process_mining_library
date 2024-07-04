6b7cedc272a4595be30a6a915ef939d4004ca5c6
Caine Merrick <cainemerrick@gmail.com>
2024-07-04 17:12:54 +0100

    developing event logs and tests files

--- event_logs/event_log.py
+++ event_logs/event_log.py
@@ -1,5 +1,9 @@
+import datetime as dt
 from pandas import DataFrame
-from typing import Optional
+from numpy import datetime64
+from typing import Iterator, Optional
+from collections.abc import MutableSequence
+
 
 def ensure_event_log_column_convention(data:DataFrame, current_name:str, convention_name:str, rename_dict:dict):
     """
@@ -7,11 +11,13 @@ def ensure_event_log_column_convention(data:DataFrame, current_name:str, convent
     if an alternative has been provided it is added to the rename_dict, if no alternative is provided then a value
     error is raised.
     """
-    if current_name not in data.columns:
-        if convention_name is None:
+    if convention_name not in data.columns:
+        if current_name is None:
             raise ValueError(f'{convention_name} not in columns and no {convention_name} column is specified')
+        elif current_name not in data.columns:
+            raise ValueError(f'{current_name} not in dataframe columns')
         rename_dict[current_name] = convention_name
-        
+
 
 class EventLog:
     """
@@ -19,13 +25,20 @@ class EventLog:
     column. Other attributes can be included but these 3 must exist. The constructor expects the following
     naming convention for the three necessary columns "case_id", "activity", "timestamp". If dataframe does 
     not conform to this convention then the names of each column must be specified in the constructor.
+
+    The timestamp column must be of the type datetime64 or dt.datetime, this is required before passing the dataframe to the 
+    constructor. This avoid the requirement to pass the current datetime format into the constructor.
+
+    The data parameter is deep copied - this ensures the original dataframe is not modified when creating 
+    an event log.
     """
 
-    def __init__(self, data:DataFrame, 
-                 case_id:Optional[str]=None, activity_name:Optional[str]=None, timestamp:Optional[str]=None):
-        
+    def __init__(self, data:DataFrame,
+                 case_id:Optional[str]=None, activity_name:Optional[str]=None, timestamp:Optional[str]=None,
+                 ):
+
         rename_dict = {}
-        
+        data = data.copy(deep=True)
         #these populate the rename dict to ensure naming convention is adhered to
         ensure_event_log_column_convention(data, case_id, 'case_id', rename_dict)
         ensure_event_log_column_convention(data, activity_name, 'activity', rename_dict)
@@ -33,18 +46,103 @@ class EventLog:
 
         data.rename(columns=rename_dict, inplace=True)
 
+        if isinstance(data['timestamp'], (datetime64, dt.datetime)):
+            raise ValueError('timestamp column must be type datetime64 or dt.datetime')
+
+        data.sort_values('timestamp', ascending=True, inplace=True)
         self.data = data
 
+    def __str__(self):
+        return self.data.__str__()
+
+
+class Trace(MutableSequence):
+    """
+    A trace is a sequence of activities. One trace represents a case where the elements in the trace represent
+    the chronological order of activities performed on that case.
+    """
+    def __init__(self, case_id:str, data:list=[]):
+        self.case_id = case_id
+        self._data = data
     
+    def __str__(self):
+        return f'CASE ID: {self.case_id} = <{",".join(self._data)}>'
+    
+    def __hash__(self) -> int:
+        return hash(self.case_id)
+    
+    def __getitem__(self, index):
+        return self._data[index]
+    
+    def __setitem__(self, index, value):
+        self._data[index] = value
+    
+    def __delitem__(self, index):
+        del self._data[index]
+    
+    def __len__(self):
+        return len(self._data)
+    
+    def insert(self, index, value):
+        self._data.insert(index, value)
+    
+    def __iter__(self) -> Iterator:
+        return self._data.__iter__()
+    
+    def __eq__(self, other):
+        if not isinstance(other, Trace):
+            return False
+        for i in range(len(self._data)):
+            if self._data[i] != other._data[i]:
+                return False
+        return True
+
+    def append(self, value):
+        self.insert(len(self), value)
 
 
+class SimplifiedEventLog:
+    """
+    A simplified event log is a multiset of traces. To build a simplified event log requires a event log to be passed 
+    in the constructor. We iterate over the rows in the event log creating traces by adding activities to the trace when 
+    we find them.
+    """
+    def __init__(self, event_log:EventLog) -> None:
+        self.traces = {}
 
-        
-        
-        
+        for i in range(len(event_log.data)):
+            event = event_log.data.iloc[i]
+            case_id = event['case_id']
+            activity = event['activity']
+            if self.traces.get(case_id) is not None:
+                trace = self.traces[case_id]
+                trace.append(activity)
+            else:
+                self.traces[case_id] = Trace(case_id, [activity])
+                
+    
+    def count_traces(self) -> dict:
+        """
+        Counts the number of occurences of each trace in the simplified event log.
+        """
+        trace_count = {}
+        for trace in self.traces.values():
+            if trace_count.get(trace) is not None:
+                trace_count[trace] += 1
+            else:
+                trace_count[trace] = 1
+        return trace_count
 
 
             
+            
+
+        
+        
+        
+
+
+
             
         
 
--- event_logs/tests.py
+++ event_logs/tests.py
@@ -1,23 +1,89 @@
 import unittest
-from event_log import EventLog, ensure_event_log_column_convention
+from event_log import EventLog, ensure_event_log_column_convention, Trace, SimplifiedEventLog
 from pandas import DataFrame
+import datetime as dt
 
 class TestEnsureColumnConvention(unittest.TestCase):
 
     def setUp(self) -> None:
+        self.rename_dict = {}
         self.convention = ['case_id', 'activity', 'timestamp']
         self.correct_df = DataFrame(columns=['case_id', 'activity', 'timestamp'])
         self.incorrect_df = DataFrame(columns=['c', 'a', 't'])
         return super().setUp()
     
+    def test_correct_column_name(self):
+        """
+        if the convetional name is already in the columns then the rename dict should remain empty
+        """
+        ensure_event_log_column_convention(self.correct_df, None, 'case_id', self.rename_dict)
+        self.assertDictEqual({}, self.rename_dict)
+
+    def test_incorrect_column_name_no_alternative(self):
+        """
+        if the convetional name is not already in the columns and the current name is not specified then a value error
+        is thrown
+        """
+        self.assertRaises(ValueError,
+                          ensure_event_log_column_convention,
+                          data = self.incorrect_df, 
+                          current_name = None, 
+                          convention_name = 'case_id', 
+                          rename_dict = self.rename_dict
+                          )
+        
+    def test_incorrect_column_name_incorrect_alternative(self):
+        """
+        if the convetional name is not already in the columns and the current name is not in the columns then a value error
+        should be thrown
+        """
+        self.assertRaises(ValueError,
+                          ensure_event_log_column_convention,
+                          data = self.incorrect_df, 
+                          current_name = 'abc', 
+                          convention_name = 'case_id', 
+                          rename_dict = self.rename_dict
+                          )
+    def test_incorrect_column_name_correct_alternative(self):
+        """
+        if the convetional name is not already in the columns and the current name is in the df columns then 
+        the rename dict should map the current name to the conventional name
+        """
+        ensure_event_log_column_convention(self.incorrect_df, 'c', 'case_id', self.rename_dict)
+        self.assertDictEqual({'c':'case_id'}, self.rename_dict)
+        
+class TestEventLogConstruction(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.convention = ['case_id', 'activity', 'timestamp']
+
+        correct_columns = {
+            'case_id':[1,2,3,4],
+            'activity':['a', 'b', 'a', 'c'],
+            'timestamp':[dt.datetime(2024, 6, 3, 11, 30, 35), dt.datetime(2024, 6, 3, 11, 30, 35), 
+                         dt.datetime(2024, 6, 3, 11, 30, 35), dt.datetime(2024, 6, 3, 11, 30, 35)]
+        }
+
+        incorrect_columns = {
+            'c':[1,2,3,4],
+            'a':['a', 'b', 'a', 'c'],
+            't':[dt.datetime(2024, 6, 3, 11, 30, 35), dt.datetime(2024, 6, 3, 11, 30, 35), 
+                         dt.datetime(2024, 6, 3, 11, 30, 35), dt.datetime(2024, 6, 3, 11, 30, 35)]
+        }
+
+        self.correct_df = DataFrame(data=correct_columns)
+        self.incorrect_df = DataFrame(data=incorrect_columns)
+        return super().setUp()
+    
     def test_correct_convention_no_columns_specified(self):
         """
-        if the dataframe columns are correctly specified and no alternative columns are given then the 
+        if the dataframe columns are correctly specified and no alternative column is given then the 
         event logs data attribute columns and the convention column names should be equal and no value errors
         should be raised
         """
         event_log = EventLog(self.correct_df)
         self.assertListEqual(self.convention, event_log.data.columns.to_list())
+        
 
     def test_correct_convention_columns_specified(self):
         """
@@ -27,12 +93,14 @@ class TestEnsureColumnConvention(unittest.TestCase):
         """
         event_log = EventLog(self.correct_df, 'case', 'ac', 'ts')
         self.assertListEqual(self.convention, event_log.data.columns.to_list())
+        
     
     def test_incorrect_convention_with_no_columns_specified(self):
         """
         if the dataframe columns are incorrect and no alternatives are given we should get a value error
         """
         self.assertRaises(ValueError, EventLog, data=self.incorrect_df)
+        
 
     def test_incorrect_convention_with_correct_columns_specified(self):
         """
@@ -42,16 +110,61 @@ class TestEnsureColumnConvention(unittest.TestCase):
         event_log = EventLog(self.incorrect_df, 'c', 'a', 't')
         self.assertListEqual(self.convention, event_log.data.columns.to_list())
     
-    def test_incorrect_convention_with_correct_columns_specified(self):
+    def test_incorrect_convention_with_incorrect_columns_specified(self):
         """
         if the dataframe columns are incorrect and the incorrect alternatives are given (i.e. no columns with the given name exist) 
         we should get a value error
         """
         self.assertRaises(ValueError, EventLog, data=self.incorrect_df, case_id='cc')
         
+class TestTraceClass(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test_trace_construction(self):
+        trace = Trace('case_1')
+        trace.append('a')
+        trace.append('b')
+        trace.append('c')
+        trace.append('d')
+        print(trace)
 
+class TestSimplifiedEventLog(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        event_data = {
+            'case_id':['a1', 'a2', 'a1', 'a2'],
+            'activity':['a', 'a', 'b', 'c'],
+            'timestamp':[dt.datetime(2024, 7, 1, 11, 20, 1), dt.datetime(2024, 7, 2, 13, 20, 1),
+                         dt.datetime(2024, 7, 3, 11, 20, 1), dt.datetime(2024, 7, 5, 11, 20, 1)]
 
+        }
+        self.data = DataFrame(data=event_data)
+        return super().setUp()
 
+    def test_sel_construction(self):
+        """
+        In this setup the simplfied event log should contain 2 traces. One for case 'a1' and one for case 'a2'.
+        The 'a1' trace should be <a, b> and the 'a2' trace should be <a, c> 
+        """
+        event_log = EventLog(self.data)
+        simplified_event_log = SimplifiedEventLog(event_log)
+        traces = simplified_event_log.traces
+
+        a1_expected_trace = Trace('a1', ['a', 'b'])
+        a2_expected_trace = Trace('a2', ['a', 'c'])
+
+        self.assertEqual(traces['a1'], a1_expected_trace)
+        self.assertEqual(traces['a2'], a2_expected_trace)
+
+    def test_count_traces(self):
+        event_log = EventLog(self.data)
+        simplified_event_log = SimplifiedEventLog(event_log)
+        trace_count = simplified_event_log.count_traces()
+        print(trace_count)
+        expected_trace_count = {Trace('any', ['a', 'b']) : 1, Trace('any2', ['a', 'c']):2}
+        self.assertDictEqual(trace_count, expected_trace_count)
 
 
 
